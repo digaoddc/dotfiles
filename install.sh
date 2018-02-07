699702aa1740f97ffb350dc4b9ed153cf74853d0
Rodrigo Coutinho <rodrigo.coutinho@locaweb.com.br>
2018-02-07 14:13:34 -0200

    Albert web search engines

--- install.sh
+++ install.sh
@@ -4,8 +4,22 @@ echo "Starting installation"
 apt-get install -y git ack htop albert build-essential ruby-dev zlib1g-dev liblzma-dev redis-server libxml2 libxml2-dev tmux docker mysql-client mysql-server
 
 # Install fzf
-#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-#~/.fzf/install
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Install docker
+# https://docs.docker.com/install/linux/docker-ee/ubuntu
+
+# Install asdf
+# https://github.com/asdf-vm/asdf
+
+# Install rbenv
+# https://github.com/rbenv/rbenv
 
 # Install thefuck
 # https://github.com/nvbn/thefuck
+
+# Install albert
+# https://albertlauncher.github.io/docs/installing/
+cp albert/albert.conf ~/.config/albert/albert.conf
+cp albert/engines.json ~/.config/albert/org.albert.extension.websearch.json
